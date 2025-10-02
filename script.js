2fd1e05967ef157d9161f13173403638c71f9241
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-10-02 10:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const fontSizeInput = document.getElementById("fontsize");
+const fontColorInput = document.getElementById("fontcolor");
+const root = document.documentElement;
+
+function parseCookie(cookieString){
+	const cookie = {}
+	cookieString = cookieString.trim();
+	if(cookieString){
+		cookieString.split(";").forEach(cookiePair => {
+			const parts = cookiePair.split("=");
+			const [key, value] = [parts?.[0].trim(), parts?.[1] || ""]
+			cookie[key] = value;
+		})
+	}
+	return cookie;
+}
+window.onload = () => {
+	const {fontcolor, fontsize} = parseCookie(document.cookie);
+	console.log(fontcolor, fontsize)
+	if(fontcolor){
+		root.style.setProperty("--fontcolor", fontcolor);
+		fontColorInput.value = fontcolor;
+	}
+	if(fontsize){
+		fontSizeInput.value = fontsize;
+		root.style.setProperty("--fontsize", `${fontsize}px`)
+	}
+}
+
+function savePreferences(){
+	const fontSize = fontSizeInput.value;
+	const fontColor = fontColorInput.value;
+	document.cookie = `fontsize=${fontSize}; path=/`;
+	document.cookie = `fontcolor=${fontColor}; path=/`;
+	
+}
+
+
+
+
